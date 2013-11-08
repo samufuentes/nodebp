5a78e015af5ef3274f5c70338e2f0ae56445c5e3
Dev <mbonet@gmail.com>
2013-11-08 20:03:28 +0100

    R

---
+++ server.js
@@ -0,0 +1,69 @@
+//setup Dependencies
+var express = require('express');
+var io = require('socket.io');
+var port = (process.env.PORT || 8881);
+var ip = (process.env.IP || '0.0.0.0');
+var env = (process.env.NODE_ENV || 'dev');
+
+//-- Setup App --
+var app = express();
+require('./config.express.js')(express, app);
+require('./config.router.js')(app);
+
+var server = app;
+/*
+//-- Setup Socket.IO --
+var io = io.listen(server);
+io.sockets.on('connection', function(socket){
+  console.log('Client Connected');
+  socket.on('message', function(data){
+    socket.broadcast.emit('server_message',data);
+    socket.emit('server_message',data);
+  });
+  socket.on('disconnect', function(){
+    console.log('Client Disconnected.');
+  });
+});
+*/
+
+///////////////////////////////////////////
+//              Routes                   //
+///////////////////////////////////////////
+
+/////// ADD ALL YOUR ROUTES HERE  /////////
+/*
+server.get('/', function(req,res){
+  res.render('index.jade', {
+    locals : { 
+              title : 'Your Page Title'
+             ,description: 'Your Page Description'
+             ,author: 'Your Name'
+             ,analyticssiteid: 'XXXXXXX' 
+            }
+  });
+});
+*/
+
+//A Route for Creating a 500 Error (Useful to keep around)
+server.get('/500', function(req, res){
+    throw new Error('This is a 500 Error');
+});
+
+//The 404 Route (ALWAYS Keep this as the last route)
+server.get('/*', function(req, res){
+    throw new NotFound;
+});
+
+function NotFound(msg){
+    this.name = 'NotFound';
+    Error.call(this, msg);
+    Error.captureStackTrace(this, arguments.callee);
+}
+
+//-- Running the server --
+app.listen(port, ip);
+console.log('Listening on http://'+ip+':' + port );
+
+//-- Expose App --
+module.exports = app;
+
