58fdf25a5574459540c8848299ba522ecb6ae06f
Dev <mbonet@gmail.com>
2013-11-08 21:11:23 +0100

    Fixing details

--- server.js
+++ server.js
@@ -26,40 +26,6 @@ io.sockets.on('connection', function(socket){
 });
 */
 
-///////////////////////////////////////////
-//              Routes                   //
-///////////////////////////////////////////
-
-/////// ADD ALL YOUR ROUTES HERE  /////////
-/*
-server.get('/', function(req,res){
-  res.render('index.jade', {
-    locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
-  });
-});
-*/
-
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
-});
-
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound;
-});
-
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
-}
-
 //-- Running the server --
 app.listen(port, ip);
 console.log('Listening on http://'+ip+':' + port );
